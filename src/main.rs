e92449be4c33af0aa805aa237797596feb379358
jatozy <52920861+jatozy@users.noreply.github.com>
2025-10-26 13:21:55 +0100

    Feature/rotate the motor a bit (#2)
    * Add hardware implementation as dependency
    
    * The motor rotates
    
    * It works also with another motor and with featrure branches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use cube_solver_hardware::cube_solver_hardware;
+
 fn main() {
-    println!("Hello, world!");
+    let mut controller = cube_solver_hardware::controller::Controller::new();
+    for _ in 0..2 {
+        let rotation_finished = controller.rotate_turntable_90_degree_clockwise();
+        rotation_finished.recv().unwrap();
+        println!("Rotation Clockwise finished.");
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        let rotation_finished = controller.rotate_turntable_90_degree_counter_clockwise();
+        rotation_finished.recv().unwrap();
+        println!("Rotation Counter Clockwise finished.");
+        std::thread::sleep(std::time::Duration::from_secs(3));
+    }
 }
