55ba75c6096fb3f0ce8ff437ef3251ae876557f3
jatozy <infrastructure@jato-engineering.de>
2025-11-14 20:25:05 +0100

    move carriage forward and backward

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use cube_solver_hardware::cube_solver_hardware;
 
 fn main() {
     let mut controller = cube_solver_hardware::controller::Controller::new();
-    for _ in 0..2 {
-        let rotation_finished = controller.rotate_turntable_90_degree_clockwise();
-        rotation_finished.recv().unwrap();
-        println!("Rotation Clockwise finished.");
-        std::thread::sleep(std::time::Duration::from_secs(3));
-        let rotation_finished = controller.rotate_turntable_90_degree_counter_clockwise();
-        rotation_finished.recv().unwrap();
-        println!("Rotation Counter Clockwise finished.");
-        std::thread::sleep(std::time::Duration::from_secs(3));
+    for _ in 0..5 {
+        let move_finished = controller.move_carriage_to_front();
+        move_finished.recv().unwrap();
+        println!("Move Carriage to Front finished.");
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        let move_finished = controller.move_carriage_to_back();
+        move_finished.recv().unwrap();
+        println!("Move Carriage to Back finished.");
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
