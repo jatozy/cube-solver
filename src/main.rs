dc47c024432269e3c2fa3506580f9a8924846fd0
jatozy <infrastructure@jato-engineering.de>
2025-11-15 09:49:33 +0100

    Continue testing the hardware

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use cube_solver_hardware::cube_solver_hardware;
 
 fn main() {
     let mut controller = cube_solver_hardware::controller::Controller::new();
-    for _ in 0..5 {
-        let move_finished = controller.move_carriage_to_front();
+    for _ in 0..2 {
+        let mut move_finished = controller.move_carriage_to_front();
         move_finished.recv().unwrap();
-        println!("Move Carriage to Front finished.");
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        let move_finished = controller.move_carriage_to_back();
+        move_finished = controller.rotate_turntable_90_degree_clockwise();
         move_finished.recv().unwrap();
-        println!("Move Carriage to Back finished.");
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        move_finished = controller.move_carriage_to_back();
+        move_finished.recv().unwrap();
+        move_finished = controller.rotate_turntable_90_degree_counter_clockwise();
+        move_finished.recv().unwrap();
     }
 }
