0d2cb79fa1c7aaf7c84814985597b54d1f45db35
jatozy <52920861+jatozy@users.noreply.github.com>
2025-11-15 10:11:53 +0100

    Feature/rotate the motor a bit (#3)
    * Add hardware implementation as dependency
    
    * The motor rotates
    
    * It works also with another motor and with featrure branches
    
    * move carriage forward and backward
    
    * Continue testing the hardware
    
    * Bump version

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use cube_solver_hardware::cube_solver_hardware;
 fn main() {
     let mut controller = cube_solver_hardware::controller::Controller::new();
     for _ in 0..2 {
-        let rotation_finished = controller.rotate_turntable_90_degree_clockwise();
-        rotation_finished.recv().unwrap();
-        println!("Rotation Clockwise finished.");
-        std::thread::sleep(std::time::Duration::from_secs(3));
-        let rotation_finished = controller.rotate_turntable_90_degree_counter_clockwise();
-        rotation_finished.recv().unwrap();
-        println!("Rotation Counter Clockwise finished.");
-        std::thread::sleep(std::time::Duration::from_secs(3));
+        let mut move_finished = controller.move_carriage_to_front();
+        move_finished.recv().unwrap();
+        move_finished = controller.rotate_turntable_90_degree_clockwise();
+        move_finished.recv().unwrap();
+        move_finished = controller.move_carriage_to_back();
+        move_finished.recv().unwrap();
+        move_finished = controller.rotate_turntable_90_degree_counter_clockwise();
+        move_finished.recv().unwrap();
     }
 }
